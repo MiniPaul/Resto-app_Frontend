374801c62479ccbd1a799d0990add98a1477ac26
Pauline Taillet <107178332+paulinetaillet@users.noreply.github.com>
2022-09-05 17:16:54 +0200

    Update Auth.overlays.js

--- Components/HomeScreen/Auth.overlays.js
+++ Components/HomeScreen/Auth.overlays.js
@@ -8,9 +8,6 @@ import { AntDesign } from '@expo/vector-icons';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 function Authentification(props) {
-  // let privateAdressIP = "172.20.10.8"; // Laurent
-  // let privateAdressIP = "172.20.10.4"; // Pauline
-  // let privateAdressIP = "192.168.1.14"; // Johann
 
   // Overlays Visibility
   const [visibleOverlaySub, setVisibleOverlaySub] = useState(false);
